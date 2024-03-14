eff439f53915c617c3df9ccb7441ec339d29659d
snehabansal2003 <123202050+snehabansal2003@users.noreply.github.com>
2024-03-14 21:32:45 +0530

    update files

---
+++ sneha_bansal/script.js
@@ -0,0 +1,9 @@
+
+function myFunction(){
+    var x= document.getElementById("topnav");
+    if (x.className==="topnav"){
+        x.className+="responsive";
+    }else{
+        x.className="topnav";
+    }
+}
